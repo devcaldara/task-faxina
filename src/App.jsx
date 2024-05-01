c9d1d1942a719e4a40bf14842577076a36d5d95a
pedro-caldara <devcaldara@gmail.com>
2024-05-01 15:43:23 -0300

    1st commit, before creating the tasks

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import KanbanBoard from './assets/Components/KanbanBoard'
+
+function App() {
+
+  return (
+    <div className="App">
+      <KanbanBoard/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/Components/KanbanBoard.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import {DragDropContext} from "react-beautiful-dnd"
+import Column from "./Colum";
+
+export default function KanbanBoard(){
+    const[completed, setCompleted] = useState([]);
+    const[incomplete, setIncomplete] = useState([]);
+    
+    return (
+        <DragDropContext>
+            <h2 style={{textAlign: "center"}}>PROGRESS BOARD</h2>
+
+                <div 
+                    style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        flexDirection: "row"
+                    }}
+                >
+                    
+                <Column title={"TO DO"} tasks={incomplete} id={"1"} />
+                    
+                </div>
+        </DragDropContext>
+    );
+}
